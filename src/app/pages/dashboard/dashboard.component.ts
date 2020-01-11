b0522b93befe94cfef839bb6b53abe3f4f351005
Bojan Zivkovic <bzivkovic@gmail.com>
2020-01-11 21:27:15 +0100

    formatted comments.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -65,14 +65,14 @@ export class DashboardComponent implements OnInit {
   loadDevicesLazy(event: LazyLoadEvent) {
     this.loading = true;
 
-    //in a real application, make a remote request to load data using state metadata from event
-    //event.first = First row offset
-    //event.rows = Number of rows per page
-    //event.sortField = Field name to sort with
-    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+    // in a real application, make a remote request to load data using state metadata from event
+    // event.first = First row offset
+    // event.rows = Number of rows per page
+    // event.sortField = Field name to sort with
+    // event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+    // filters: FilterMetadata object having field as key and filter value, filter matchMode as value
 
-    //imitate db connection over a network
+    // imitate db connection over a network
     setTimeout(() => {
         if (this.datasource) {
             this.devices = this.datasource.slice(event.first, (event.first + event.rows));

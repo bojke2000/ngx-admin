5196b582d7c9146f01ea425b377e80035d6c8f22
Bojan Zivkovic <bzivkovic@gmail.com>
2020-01-26 10:38:34 +0100

    localized labels for menu items.

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -26,7 +26,7 @@ export class PagesComponent implements OnInit {
     MENU_ITEMS.forEach(item => {
       if (item.children !== undefined) {
         item.children.forEach(subitem => {
-          this.localizeItem(subitem); 
+          this.localizeItem(subitem);
         });
       }
       this.localizeItem(item);

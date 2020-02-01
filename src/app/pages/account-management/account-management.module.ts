094cbdb7c666ca3b2fd2e53782c52706d11f2629
Bojan Zivkovic <bzivkovic@gmail.com>
2020-02-01 13:24:38 +0100

    user account: dialog styling

--- src/app/pages/account-management/account-management.module.ts
+++ src/app/pages/account-management/account-management.module.ts
@@ -5,12 +5,13 @@ import { ThemeModule } from '../../@theme/theme.module';
 import {TableModule} from 'primeng/table';
 import {DialogModule} from 'primeng/dialog';
 import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {PanelModule} from 'primeng/panel';
 import {HttpClient} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { UserAccountComponent } from './user-account.component';
-
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -21,6 +22,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     NbCardModule,
     NbButtonModule,
     ButtonModule,
+    InputTextModule,
+    PanelModule,
     ThemeModule,
     TableModule,
     FormsModule,

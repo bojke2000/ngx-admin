59b4497a93c813e3e7432a761bb5bed373236c80
Bojan Zivkovic <bzivkovic@gmail.com>
2020-02-01 19:38:28 +0100

    user-account: integrated with BE.

--- src/app/pages/account-management/user-account.component.ts
+++ src/app/pages/account-management/user-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import {Table} from 'primeng/table';
+import { Table } from 'primeng/table';
 import { AbstractComponent } from '../../abstract.component';
 import { TranslateService } from '@ngx-translate/core';
 import { LazyLoadEvent } from 'primeng/api/public_api';
@@ -8,113 +8,127 @@ import { UserAccountService } from '../../service/user-account.service';
 
 
 @Component({
-    selector: 'user-account',
-    templateUrl: './user-account.component.html',
-    styleUrls: ['./user-account.css'],
-  })
-  export class UserAccountComponent extends AbstractComponent implements OnInit {
-    userAccounts: UserAccount[];
-    userAccount: UserAccount;
-    selectedUserAccount: UserAccount;
-    newUserAccount: boolean;
-    displayDialog: boolean;
-    datasource: UserAccount[];
-    totalRecords: number;
-    cols: any[];
-    loading: boolean;
-  @ViewChild('table', {static: false}) table: Table;
+  selector: 'user-account',
+  templateUrl: './user-account.component.html',
+  styleUrls: ['./user-account.css'],
+})
+export class UserAccountComponent extends AbstractComponent implements OnInit {
+  userAccounts: UserAccount[];
+  userAccount: UserAccount;
+  selectedUserAccount: UserAccount;
+  newUserAccount: boolean;
+  displayDialog: boolean;
+  datasource: UserAccount[];
+  totalRecords: number;
+  cols: any[];
+  loading: boolean;
+  @ViewChild('table', { static: false }) table: Table;
 
-    constructor(private userAccountservice: UserAccountService, translate: TranslateService) {
-      super(translate);
-    }
+  constructor(private userAccountservice: UserAccountService, translate: TranslateService) {
+    super(translate);
+  }
 
-    ngOnInit(): void {
-      this.userAccountservice.getUserAccounts().then(userAccounts => {
-        this.datasource = userAccounts;
-        this.totalRecords = this.datasource.length;
-      }) ;
-      this.cols = [
-        { field: 'id', header: 'ID', width: '70px' },
-        { field: 'username', header: 'Name' , width: '120px' },
-        { field: 'password', header: 'Password' , width: '120px' },
-        { field: 'email', header: 'Email' , width: '150px' },
-        { field: 'city', header: 'City' , width: '150px' },
-        { field: 'accountType', header: 'Account Type' , width: '120px' },
-        { field: 'active', header: 'Status' , width: '120px' },
-        { field: 'lastLogin', header: 'Last Login' , width: '120px' },
-      ];
-      this.loading = true;
-    }
+  ngOnInit(): void {
+    this.userAccountservice.getUserAccounts().then(userAccounts => {
+      this.datasource = userAccounts;
+      this.totalRecords = this.datasource.length;
+    });
+    this.cols = [
+      { field: 'id', header: 'ID', width: '70px' },
+      { field: 'username', header: 'Name', width: '120px' },
+      { field: 'password', header: 'Password', width: '120px' },
+      { field: 'email', header: 'Email', width: '150px' },
+      { field: 'city', header: 'City', width: '150px' },
+      { field: 'accountType', header: 'Account Type', width: '120px' },
+      { field: 'active', header: 'Status', width: '120px' },
+      { field: 'lastLogin', header: 'Last Login', width: '120px' },
+    ];
+    this.loading = true;
+  }
 
-    loadUserAccountsLazy(event: LazyLoadEvent) {
-      this.loading = true;
-      setTimeout(() => {
-          if (this.datasource) {
-              this.userAccounts = this.datasource.slice(event.first, (event.first + event.rows));
-              this.loading = false;
-          }
-      }, 1000);
-    }
+  loadUserAccountsLazy(event: LazyLoadEvent) {
+    this.loading = true;
+    setTimeout(() => {
+      if (this.datasource) {
+        this.userAccounts = this.datasource.slice(event.first, (event.first + event.rows));
+        this.loading = false;
+      }
+    }, 1000);
+  }
 
-    resetSort() {
-      this.table.sortOrder = 0;
-      this.table.sortField = '';
-      this.table.reset();
-     }
+  resetSort() {
+    this.table.sortOrder = 0;
+    this.table.sortField = '';
+    this.table.reset();
+  }
 
-     showDialogToAdd() {
-      this.newUserAccount = true;
-      this.userAccount = { id: undefined,
-        username: undefined,
-        password: undefined,
-        email: undefined,
-        city: undefined,
-        accountType: undefined,
-        active: undefined,
-        lastLogin: undefined};
-      this.displayDialog = true;
+  showDialogToAdd() {
+    this.newUserAccount = true;
+    this.userAccount = {
+      id: undefined,
+      username: undefined,
+      password: undefined,
+      email: undefined,
+      city: undefined,
+      accountType: undefined,
+      active: undefined,
+      lastLogin: undefined,
+    };
+    this.displayDialog = true;
   }
 
   save() {
-      const userAccounts = [...this.userAccounts];
-      if (this.newUserAccount)
-          userAccounts.push(this.userAccount);
-      else
-          userAccounts[this.userAccounts.indexOf(this.selectedUserAccount)] = this.userAccount;
+    const userAccounts = [...this.userAccounts];
 
-      this.userAccounts = userAccounts;
-      this.userAccount = null;
-      this.displayDialog = false;
+    if (this.newUserAccount) {
+      this.userAccountservice.addUserAccount(this.userAccount).subscribe(ua => {
+        userAccounts.push(ua);
+      });
+    } else {
+      this.userAccountservice.updateUserAccount(this.userAccount).subscribe(ua => {
+        userAccounts[this.userAccounts.indexOf(this.selectedUserAccount)] = this.userAccount;
+      });
+    }
+
+    this.userAccounts = userAccounts;
+    this.userAccount = null;
+    this.displayDialog = false;
   }
 
   delete() {
-      const index = this.userAccounts.indexOf(this.selectedUserAccount);
-      this.userAccounts = this.userAccounts.filter((val, i) => i !== index);
-      this.userAccount = null;
-      this.displayDialog = false;
+    const index = this.userAccounts.indexOf(this.selectedUserAccount);
+    if (index !== 0) {
+      this.userAccountservice.deleteUserAccount(this.userAccount).subscribe(ua => {
+        this.userAccounts = this.userAccounts.filter((val, i) => i !== index);
+      });
+    }
+    this.userAccount = null;
+    this.displayDialog = false;
   }
 
   onRowSelect(event) {
-      this.newUserAccount = false;
-      this.userAccount = this.cloneUserAccount(event.data);
-      this.displayDialog = true;
+    this.newUserAccount = false;
+    this.userAccount = this.cloneUserAccount(event.data);
+    this.displayDialog = true;
   }
 
   cloneUserAccount(c: UserAccount): UserAccount {
-      const userAccount = { id: undefined,
-                            username: undefined,
-                            password: undefined,
-                            email: undefined,
-                            city: undefined,
-                            accountType: undefined,
-                            active: undefined,
-                            lastLogin: undefined};
+    const userAccount = {
+      id: undefined,
+      username: undefined,
+      password: undefined,
+      email: undefined,
+      city: undefined,
+      accountType: undefined,
+      active: undefined,
+      lastLogin: undefined,
+    };
 
-      for (const prop in c) {
-        if (c[prop] !== undefined) {
-          userAccount[prop] = c[prop];
-        }
+    for (const prop in c) {
+      if (c[prop] !== undefined) {
+        userAccount[prop] = c[prop];
       }
-      return userAccount;
+    }
+    return userAccount;
   }
-  }
+}
--- src/app/service/user-account.service.ts
+++ src/app/service/user-account.service.ts
@@ -1,20 +1,61 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserAccount } from '../domain/user-account';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class UserAccountService {
 
+    private userAccountUrl = 'http://localhost:8081/user-accounts';
+    private httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      }),
+    };
+
     constructor(private http: HttpClient) { }
 
     getUserAccounts() {
-    return this.http.get<any>('http://localhost:8081/user-accounts')
-      .toPromise()
-      .then(res => <UserAccount[]>res.data)
-      .then(data => data);
+      return this.http.get<any>(this.userAccountUrl)
+        .toPromise()
+        .then(res => <UserAccount[]>res.data)
+        .then(data => data);
     }
 
-    getAccounts() {
+    addUserAccount (userAccount: UserAccount): Observable<UserAccount> {
+      return this.http.post<UserAccount>(this.userAccountUrl, userAccount, this.httpOptions)
+        .pipe(catchError(this.handleError));
+    }
+
+    updateUserAccount (userAccount: UserAccount): Observable<UserAccount> {
+      return this.http.put<UserAccount>(this.userAccountUrl, userAccount, this.httpOptions)
+        .pipe(catchError(this.handleError));
+    }
+
+    deleteUserAccount (userAccount: UserAccount): Observable<UserAccount> {
+      const url = `${this.userAccountUrl}/${userAccount.id}`; // DELETE api/heroes/42
+      return this.http.delete<UserAccount>(url, this.httpOptions)
+      .pipe(catchError(this.handleError));
+    }
+
+    private handleError(error: HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      // return an observable with a user-facing error message
+      return throwError(
+        'Something bad happened; please try again later.');
+    }
+
+    getTestAccounts() {
       return this.http.get<any>('assets/showcase/data/user-accounts.json')
         .toPromise()
         .then(res => <UserAccount[]>res.data)

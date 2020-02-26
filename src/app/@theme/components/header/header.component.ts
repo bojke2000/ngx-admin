14dee39bf5d44b8dd3307e31daef7d32975d42d5
Bojan Zivkovic <bzivkovic@gmail.com>
2020-02-26 23:39:57 +0100

    Device renamed to UserCard.

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -67,7 +67,7 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
     this.userService.getUsers()
       .pipe(takeUntil(this.destroy$))
       .subscribe((users: any) => this.user.picture = users.nick.picture);
-    
+
     const { xl } = this.breakpointService.getBreakpointsMap();
     this.themeService.onMediaQueryChange()
       .pipe(

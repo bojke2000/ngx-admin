6e6ccf003a53f6ab862af6642217d3950c129bd8
Bojan Zivkovic <bzivkovic@gmail.com>
2020-03-26 16:32:47 +0100

    import user card: client will send column and value from card_column to the BE.

--- src/app/pages/import-export/import-user-card.component.ts
+++ src/app/pages/import-export/import-user-card.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { MessageService, SelectItem } from 'primeng/api';

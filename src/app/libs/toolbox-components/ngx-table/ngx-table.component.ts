ba60cd614c81f7aab789b386ab569ad36d5e586a
Bojan Zivkovic <bzivkovic@gmail.com>
2020-04-17 14:15:31 +0200

    ngx-table - new component.

---
+++ src/app/libs/toolbox-components/ngx-table/ngx-table.component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { AbstractComponent } from '../../../AbstractComponent';
+import { of } from 'rxjs';
+import { tap, distinctUntilChanged } from 'rxjs/operators';
+
+@Component({
+  selector: 'ngx-table',
+  templateUrl: './ngx-table.component.html',
+  styleUrls: ['./ngx-table.component.scss'],
+})
+export class NgxTableComponent extends AbstractComponent {
+  @Input()
+  columns: any[];
+  @Input()
+  value: [];
+  @Input()
+  selection: any;
+  @Input()
+  totalRecords: number;
+  @Input()
+  loading = true;
+  @Input()
+  dataKey: string;
+  @Output()
+  lazyLoad: EventEmitter<any> = new EventEmitter();
+  @Output()
+  rowSelect: EventEmitter<any> = new EventEmitter();
+  @Output()
+  rowUnselect: EventEmitter<any> = new EventEmitter();
+
+  constructor(translate: TranslateService) {
+    super(translate);
+  }
+
+  get loading$() {
+    return of(this.loading).pipe(
+      tap(loading => console.log("@@@@@ LOADING", loading)),
+      distinctUntilChanged(),
+    );
+  }
+
+  get value$() {
+    return of(this.value).pipe(
+      distinctUntilChanged(),
+    );
+  }
+
+  onLazyLoad(event: any): void {
+    this.lazyLoad.emit(event);
+  }
+
+  onRowSelect(event: any): void {
+    this.rowSelect.emit(event);
+  }
+
+  onRowUnselect(event: any): void {
+    this.rowUnselect.emit(event);
+  }
+
+}

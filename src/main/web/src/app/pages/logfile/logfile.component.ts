c47e761e47b822efcbf2a894fc56af7a82982c6e
Bojan Zivkovic <bzivkovic@gmail.com>
2020-09-20 22:03:55 +0200

    activity log: added search capabilities.

--- src/main/web/src/app/pages/logfile/logfile.component.ts
+++ src/main/web/src/app/pages/logfile/logfile.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractComponent } from '../../AbstractComponent';
 import { LogfileService } from '../../service/logfile.service';
 import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'ngx-logfile',
@@ -11,6 +12,8 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LogfileComponent extends AbstractComponent implements OnInit {
   logfile: string[] = undefined;
+  activity: string[] = undefined;
+  activitySearch: string = undefined;
 
   constructor(
     private logfileService: LogfileService,
@@ -18,12 +21,30 @@ export class LogfileComponent extends AbstractComponent implements OnInit {
     super(translate);
   }
 
+  get activity$() {
+    return of(this.activity);
+  }
+
   ngOnInit(): void {
     this.logfileService.getLogfile().subscribe(logfile => {
       let tempbuf: string[] = (logfile as string).split('\n');
       this.logfile = tempbuf.length <= 2000 ? tempbuf.reverse() : tempbuf.slice(tempbuf.length - 2000).reverse();
-      tempbuf = [];
+      this.activity = this.logfile;
     });
   }
 
+  private filter(): void {
+    const {activitySearch, logfile} = this;
+    this.activity = activitySearch && activitySearch !== '' ? logfile.filter(line => line.toLowerCase().includes(activitySearch.toLowerCase())) : logfile;
+  }
+
+  onActivitySearch(event: any) {
+    if (event.keyCode === 13) {
+      this.filter();
+    }
+  }
+
+  onActivitySearchBlur() {
+    this.filter();
+  }
 }

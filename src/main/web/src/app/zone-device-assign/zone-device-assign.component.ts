d59a733e7673ae3ca4b820af750a31628383dacb
Bojan Zivkovice <bzivkovic@gmail.com>
2024-12-21 16:12:50 +0100

    zone device assign added drop-down to host zone devices

--- src/main/web/src/app/zone-device-assign/zone-device-assign.component.ts
+++ src/main/web/src/app/zone-device-assign/zone-device-assign.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractComponent } from '../AbstractComponent';
 import { TranslateService } from '@ngx-translate/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'ngx-zone-device-assign',
@@ -18,6 +19,7 @@ export class ZoneDeviceAssignComponent extends AbstractComponent implements OnIn
   currentPageAssigned = 1;
   totalPagesAvailable = 0;
   totalPagesAssigned = 0;
+  zoneDevices: SelectItem[] = [];
 
   constructor(translate: TranslateService) {
     super(translate);
